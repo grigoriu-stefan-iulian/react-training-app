86e35952128de0c46c4440d656b5a0c0d096cab1
grigoriu-stefan-iulian <grigoriu.stefan_iulian@yahoo.com>
2019-02-18 20:01:25 +0200

    Second commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # react-training-app
 Learning React
+
+Made with "create-react-app"
+
+run: 
+npm install
+npm start
+
+or:
+
+yarn insta
+yarn startll
+
--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   state = {
     clicked: 0,
     selectedColor: {
-      hexCode: 'grey',
+      hexCode: 'greytt',
       label: 'greyLabel'
     }
   }
